02ac85d8892fdfee09e5bb5793a8657b3fd9a715
RobertoSpy <152715649+RobertoSpy@users.noreply.github.com>
2024-03-08 20:26:16 +0200

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ import { styles } from "../styles";
 import { services } from "../constants";
 import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
-///la div ul cu option am facut sa para ca un card
+
 const ServiceCard = ({ index, title, icon }) => (
   <Tilt className='xs:w-[250px] w-full'>
     <motion.div
